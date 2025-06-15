db18d46a262d113cb68427929e8b6c0d29b13529
SantinoGullacci <166406463+SantinoGullacci@users.noreply.github.com>
2025-06-15 15:18:43 -0300

    Delete Práctico 4 Entrega directory

--- Práctico 4 Entrega/tp4backend/controllers/personas.controllers.js
+++
@@ -1,9 +0,0 @@
-const personas = require('../models/personas.models.js');
-
-const getPersonas = (req, res) => {
-  res.json(personas);
-};
-
-module.exports = {
-  getPersonas
-};
--- Práctico 4 Entrega/tp4backend/routes/personas.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getPersonas} = require('../controllers/personas.controllers.js');
-
-router.get('/', getPersonas);
-
-module.exports = router;
