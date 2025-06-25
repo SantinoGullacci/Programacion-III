c6f7eb9b7c2afb00fdb79ef7498e4e55ff9ecf51
SantinoGullacci <santinogullacci@gmail.com>
2025-06-25 18:30:26 -0300

    Actualizo Corrección TP3

--- Práctico 3 Entrega Corrección/src/controllers/turnosWeb.controller.js
+++ Práctico 3 Entrega Corrección/src/controllers/turnosWeb.controller.js
@@ -1,37 +1,38 @@
 const turnosModel = require("../models/mock/turnos.models.js");
 
-class TurnosController {
-  mostrarTurnos = async (req, res) => {
+class TurnosWebController {
+  async mostrarTurnos(req, res) {
     try {
       const turnos = await turnosModel.getTurnos();
       res.render("turnos", { turnos });
     } catch (error) {
       res.status(500).send(error.message);
     }
-  };
-  
+  }
 
   formularioNuevoTurno(req, res) {
     res.render("nuevo-turno");
   }
 
-  crearTurno(req, res) {
-    const { idPaciente, fecha, hora, motivo } = req.body;
-    turnosModel.agregarTurno({ idPaciente, fecha, hora, motivo });
-    res.redirect("/turnos");
-  }
-
-  cancelarTurno(req, res) {
-    const { id } = req.params;
-    const eliminado = turnosModel.deleteTurno(id);
-  
-    if (eliminado) {
+  async crearTurno(req, res) {
+    try {
+      const { idPaciente, fecha, hora, motivo } = req.body;
+      await turnosModel.agregarTurno({ idPaciente, fecha, hora, motivo });
       res.redirect("/turnos");
-    } else {
-      res.status(404).send("Turno no encontrado");
+    } catch (error) {
+      res.status(500).send(error.message);
     }
   }
 
-}  
+  async cancelarTurno(req, res) {
+    try {
+      const { id } = req.params;
+      await turnosModel.deleteTurno(id);
+      res.redirect("/turnos");
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  }
+}
 
-module.exports = new TurnosController();
+module.exports = new TurnosWebController();
